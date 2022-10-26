eebb9b63d3a16c134cd6b7e8e3e84a19b44bb627
Carlos Tarjano <tesserato@hotmail.com>
2022-10-26 07:08:50 -0300

    modified to take examples from the dataset

--- s04_inference.py
+++ s04_inference.py
@@ -3,10 +3,19 @@ from s02_transformerDefinition import *
 transformer = torch.load("transformer.pt")
 transformer.eval()
 
-raw_pt = "mas e se estes fatores fossem ativos ?"
+
+IDX = 2
+print(">> teste de tradução:")
+ipt_pt = src_vocab.lookup_tokens([id for id in src_val_tensor[IDX] if id != PAD_IDX])
+ipt_pt = " ".join([t for t in ipt_pt if t not in special_symbols])
+print("src:", ipt_pt)
+tgt_en = tgt_vocab.lookup_tokens([id for id in tgt_val_tensor[IDX] if id != PAD_IDX])
+tgt_en = " ".join([t for t in tgt_en if t not in special_symbols])
+print("tgt:", tgt_en)
+
 max_length = 100
 model = transformer
-input_sequence = torch.tensor([[BOS_IDX] + src_vocab(toTokens(raw_pt.rstrip("\n"))) + [EOS_IDX]])
+input_sequence = torch.tensor([[BOS_IDX] + src_vocab(toTokens(ipt_pt.rstrip("\n"))) + [EOS_IDX]])
 
 y_input = torch.tensor([[BOS_IDX]], dtype=torch.long, device=DEVICE)
 
@@ -16,14 +25,15 @@ idxs = []
 for _ in range(max_length):
     src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(input_sequence, y_input)
 
-    # logits = transformer(input_sequence, y_input, None, None, None, None, None)
-    logits = transformer(input_sequence, y_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
-    print(logits[0].size())
-    print(torch.argmax(logits[0], 1))
+    logits = transformer(input_sequence, y_input, None, None, None, None, None)
+    # logits = transformer(input_sequence, y_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
+
+    # print(logits[0].size())
+    # print(torch.argmax(logits[0], 1))
     next_item = logits.topk(1)[1].view(-1)[-1].item() # num with highest probability
     next_item = torch.tensor([[next_item]], device=DEVICE)
     idxs.append(next_item.item())
-    print(idxs[-1])
+    # print(idxs[-1])
 
     # Concatenate previous input with predicted best word
     y_input = torch.cat((y_input, next_item), dim=1)
@@ -33,8 +43,8 @@ for _ in range(max_length):
       break
 
 
-print(">> teste de tradução:")
+
 tokens = tgt_vocab.lookup_tokens(idxs)
-print(tokens)
+# print(tokens)
 string = " ".join([t for t in tokens if t not in special_symbols])
-print(string)
+print("inf:", string)
